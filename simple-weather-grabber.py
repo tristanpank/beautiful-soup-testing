52b260fbc7e41d7844bfc61037e1b824187a4a75
tristanpank <tristanpank@gmail.com>
2022-07-19 17:18:01 -0400

    Made Weather Grabber for my home data
    Using the requests module and beautiful soup
    this program collects general weather data
    by searching for specific HTML classes on dark
    sky website.

--- simple-weather-grabber.py
+++ simple-weather-grabber.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.YtcbXnbMKUk")
+page = requests.get("https://darksky.net/forecast/29.0721,-82.193/us12/en")
 soup = BeautifulSoup(page.content, 'html.parser')
+weather_status = soup.find(class_="summary swap").get_text()
+feels_like = soup.find(class_="feels-like-text").get_text()
+humidity = soup.find(class_="num swip humidity__value").get_text()
+uv_index = soup.find(class_="num uv__index__value").get_text()
 
-current_temp = soup.find(class_="myforecast-current-lrg").get_text()
-print(current_temp)
+print(f"General Forecast: {weather_status}")
+print(f"Feels Like: {feels_like}")    
+print(f"Humidity {humidity}%")
+print(f"UV Index: {uv_index}")
