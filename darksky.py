df109d4fd9ec1c0ccf39b5ba04e2f8e4bb27a851
tristanpank <tristanpank@gmail.com>
2022-09-04 10:56:31 -0400

    Fix Program for Selenium Update
    Went through and made edits to all of the code involving
    Selenium, as there was an update that changed syntax for it.

--- darksky.py
+++ darksky.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 
 def get_new_url(location):
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(options=op)
+    # op = webdriver.ChromeOptions()
+    # op.add_argument('headless')
+    driver = webdriver.Chrome("C:/Users/trist/Downloads/chromedriver_win32/chromedriver")
     driver.get("https://darksky.net/forecast/29.0721,-82.193/us12/en")
 
-    parent = driver.find_element_by_id("searchForm")
-    input_element = parent.find_element_by_tag_name("input")
-    search_button = driver.find_element_by_class_name("searchButton")
+    parent = driver.find_element(By.ID, "searchForm")
+    input_element = parent.find_element(By.TAG_NAME, "input")
+    search_button = driver.find_element(By.CLASS_NAME, "searchButton")
 
     input_element.clear()
     input_element.send_keys(location)
